b82a1733d9c09c06f0233b3bf47639f3f8dc9c42
feitan <caf43rer@gmail.com>
2024-05-27 11:10:42 +0800

    log4j日志导入数据库

--- src/lib.rs
+++ src/lib.rs
@@ -5,21 +5,108 @@ use elasticsearch::{
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use serde_json::Value;
 
+use regex::Regex;
+use lazy_static::lazy_static;
+use std::{collections::HashMap, str::FromStr};
+
+
+lazy_static! {
+    static ref LOG_PATTERN: Regex = Regex::new(
+        r"(?x)
+        ^(?P<timestamp>\d{2}:\d{2}:\d{2}\.\d{3})\s+
+        \[(?P<thread>[^\]]+)\]\s+
+        (?P<loglevel>\w+)\s+
+        (?P<class>[^\s]+)\s+-\s+
+        \[(?P<context>[^\]]+)\]\s+-\s+
+        (?P<message>.*?)
+        (\n\s*at\s+(?P<stack>.+))?
+        $"
+    ).unwrap();
+
+    static ref FILE_NAME_PATTERN: Regex = Regex::new(
+        r"(?x)
+        ^(?P<loglevel>\w+)\.
+        (?P<date>\d{4}-\d{2}-\d{2})\.log$
+        "
+    ).unwrap();
+}
+
+//文件名为 info.2024-03-27.log  error.2024-03-27.log
+pub fn read_date_from_file_name(file_name: &str) -> (String, String) {
+    let captures = FILE_NAME_PATTERN.captures(file_name).unwrap();
+    (captures["date"].to_string(), captures["loglevel"].to_string())
+}
+
+
+pub async fn read_from_file(file_path: &str) -> Result<String, Error> {
+    let content = tokio::fs::read_to_string(file_path).await?;
+    Ok(content)
+}
+
+
+pub fn parse_log_line(line: &String,row: u32,date:&String) -> Option<MyLogRecord> {
+    if let Some(captures) = LOG_PATTERN.captures(line.as_str()) {
+        let mut log_data = HashMap::new();
+        log_data.insert("timestamp".to_string(), captures["timestamp"].to_string());
+        log_data.insert("thread".to_string(), captures["thread"].to_string());
+        log_data.insert("loglevel".to_string(), captures["loglevel"].to_string());
+        log_data.insert("class".to_string(), captures["class"].to_string());
+        log_data.insert("context".to_string(), captures["context"].to_string());
+        log_data.insert("message".to_string(), captures["message"].to_string());
+        
+        if let Some(stack) = captures.name("stack") {
+            log_data.insert("stack".to_string(), stack.as_str().to_string());
+        }
+        let key = format!("{}_{}", &log_data["timestamp"],row);
+        let mut stack = String::from_str("").unwrap();
+
+        if let Some(s) = log_data.get("stack") {
+            stack = s.to_string();
+        }
+
+        let timestamp = format!("{} {}",date,&log_data["timestamp"]);
+
+        let record = MyLogRecord::new(
+            key,
+            timestamp,
+            &log_data["thread"],
+            &log_data["loglevel"],
+            &log_data["class"],
+            &log_data["context"],
+            &log_data["message"],
+            stack,
+        );
+        
+
+        Some(record)
+    } else {
+        None
+    }
+}
+
+
+
+
 pub trait EsEntity {
     fn index() -> String;
     fn id(&self) -> &str;
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct MyDocument {
+pub struct MyLogRecord {
     key: String,
-    size: i32,
-    content: String,
+    timestamp: String,
+    thread: String,
+    loglevel: String,
+    class: String,
+    context: String,
+    message: String,
+    stack: String,
 }
 
-impl EsEntity for MyDocument {
+impl EsEntity for MyLogRecord {
     fn index() -> String {
-        String::from("my_index")
+        String::from("my_log_index")
     }
     fn id(&self) -> &str {
         &self.key
@@ -27,12 +114,17 @@ impl EsEntity for MyDocument {
 
 }
 
-impl MyDocument {
-    pub fn new(key: &str, size: i32, content: &str) -> MyDocument {
-        MyDocument {
-            key: key.to_string(),
-            size: size,
-            content: content.to_string(),
+impl MyLogRecord {
+    pub fn new(key: String, timestamp: String, thread: &str, loglevel: &str, class: &str, context: &str, message: &str, stack: String) -> Self {
+        MyLogRecord {
+            key: key,
+            timestamp: timestamp.to_string(),
+            thread: thread.to_string(),
+            loglevel: loglevel.to_string(),
+            class: class.to_string(),
+            context: context.to_string(),
+            message: message.to_string(),
+            stack: stack
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+
+use std::str::FromStr;
+
 use elasticsearch::{
     http:: transport::Transport,
     Elasticsearch, Error
 };
-use serde_json::json;
-use myesrs::{MyDocument, search_doc};
 use dotenv::dotenv;
+use myesrs::{read_from_file,parse_log_line,read_date_from_file_name,insert_doc};
+
 
 
 #[tokio::main]
@@ -13,13 +16,55 @@ async fn main() -> Result<(), Error> {
     let es_url = std::env::var("ES_URL").expect("ES_URL must be set");
     let transport = Transport::single_node(es_url.as_str()).unwrap();
     let client = Elasticsearch::new(transport);
-    let query = json!({
-        "query":{
-            "match_all":{}
+    let file_name = "samle.2024-05-22.log";
+    let file_path = String::from("./") + file_name;
+    let (date, _loglevel) = read_date_from_file_name(file_name);    
+    let content =  read_from_file(file_path.as_str()).await?;
+    //content用\n 换行符切割字符串
+    let content = content.split("\n");
+    let mut log_str = String::new();
+    let mut row_num = 0;
+    let mut log_data = Vec::new();
+    for line in content {
+        //如果line是\t开头
+        if line.starts_with("\t") {
+            //去掉\t
+            log_str.push_str(line.trim_start_matches("\t"));
+            log_str.push_str(" \n ");
+            continue;
         }
-    });
-    let res: Vec<MyDocument> = search_doc(&client,query).await?;
-    print!("{:?}", res);
+        //如果log_str不为空
+        log_str = String::from_str(line).unwrap();
+        let log = parse_log_line(&log_str,row_num,&date);
+        match log {
+            Some(log) => {
+                log_data.push(log);
+            },
+            None => {
+                println!("No match");
+            }
+        }
+        row_num += 1;
+    }
+
+    println!("{:?}", log_data);
+    for data in log_data {
+        let res = insert_doc(&client,&data).await?;
+        println!("{:?}", res);
+    }
+    //如果log_str不为空
+    // if !log_str.is_empty() {
+    //     println!("{:?}", log_str);
+    //     let log = parse_log_line(&log_str,row_num);
+    //     match log {
+    //         Some(log) => {
+    //             println!("{:?}", log);
+    //         },
+    //         None => {
+    //             println!("No match");
+    //         }
+    //     }
+    // }
 
     Ok(())
 }
